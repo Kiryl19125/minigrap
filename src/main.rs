2cc1d483c4ffee70cd0beecc67f733852a5a0fa5
Kiryl19125 <zhydalovich.kiryl@gmail.com>
2023-09-30 19:06:21 +0200

    Added lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,18 @@
 use std::env;
-use std::fs;
-
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem with parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Serach for {}", config.query);
     println!("In file {}", config.file_path);
-    println!("File content:\n{}", read_file(&config.file_path));
-    // config::show_unfo();
-    
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config {query, file_path}
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
     }
 }
-
-
-fn read_file(file_path: &str) -> String {
-    let content = fs::read_to_string(file_path).expect("Error while reading file");
-
-    content
-}
-// fn get_args() -> Vec<String> {
-//     let args: Vec<String> = env::args().collect();
-//     args
-// }
-//
-// fn read_file_content(file_path: &str) -> String {
-//     let content = fs::read_to_string(file_path).
-//         expect("Error while reading file");
-//     content
-// }
