9b75b248e3000c0b5df7666c556c52fb9e2c7c4d
Kiryl19125 <zhydalovich.kiryl@gmail.com>
2023-09-30 18:17:33 +0200

    Second Commit

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,45 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-    
-    // for &arg in args {
-    //     println!("{}", arg);
-    // }
-    
-    let query = &args[1];
-    let file_path = &args[2];
+    let config: Config = Config::new(&args);
 
-    
-    println!("Searching for = {}", {query});
-    println!("In file = {}", {file_path});
-
-    
-    let content = fs::read_to_string(file_path).
-        expect("Error while rading file");
-
-    println!("With text:\n{}", content);
+    println!("Serach for {}", config.query);
+    println!("In file {}", config.file_path);
+    println!("File content:\n{}", read_file(&config.file_path));
+    // config::show_unfo();
     
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {query, file_path}
+    }
+}
+
+
+fn read_file(file_path: &str) -> String {
+    let content = fs::read_to_string(file_path).expect("Error while reading file");
+
+    content
+}
+// fn get_args() -> Vec<String> {
+//     let args: Vec<String> = env::args().collect();
+//     args
+// }
+//
+// fn read_file_content(file_path: &str) -> String {
+//     let content = fs::read_to_string(file_path).
+//         expect("Error while reading file");
+//     content
+// }
