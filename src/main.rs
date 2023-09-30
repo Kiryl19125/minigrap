57446d690b70e2456017311be736d5b86741154b
Kiryl19125 <zhydalovich.kiryl@gmail.com>
2023-09-30 16:54:46 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    
+    // for &arg in args {
+    //     println!("{}", arg);
+    // }
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    
+    println!("Searching for = {}", {query});
+    println!("In file = {}", {file_path});
+
+    
+    let content = fs::read_to_string(file_path).
+        expect("Error while rading file");
+
+    println!("With text:\n{}", content);
+    
+}
