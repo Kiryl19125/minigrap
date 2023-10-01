6483265c40665582e6ed16d038148c34c1845798
Kiryl19125 <zhydalovich.kiryl@gmail.com>
2023-10-01 20:01:08 +0200

    Added case insensetive search and env variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::fs;
 use colored::*;
+use std::env;
 use std::error::Error;
 
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,13 +17,14 @@ impl Config {
         } else {
             let query = args[1].clone();
             let file_path = args[2].clone();
-            return Ok(Config{query, file_path})
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
+            return Ok(Config{query, file_path, ignore_case})
         }
     }
 }
 
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut result = Vec::new();
 
     for line in contents.lines() {
@@ -32,13 +35,47 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
-pub fn search_case_insensitive()
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            result.push(line);
+        }
+    }
+
+    result
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
+    // let contents = fs::read_to_string(config.file_path)?;
+    // 
+    // // for line in search(&config.query, &contents) {
+    // //     println!("{}", line.yellow());
+    // // }
+    //
+    // let results = if config.ignore_case {
+    //     search_case_insensitive(&config.query, &contents);
+    // } else {
+    //     search_case_sensitive(&config.query, &contents);
+    // };
+    // 
+    // for line in results {
+    //     println!("{}", line.yellow());
+    // }
+    //
+    // Ok(())
+
     let contents = fs::read_to_string(config.file_path)?;
-    
-    for line in search(&config.query, &contents) {
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search_case_sensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line.yellow());
     }
 
@@ -57,7 +94,7 @@ Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));       
+        assert_eq!(vec!["safe, fast, productive."], search_case_sensitive(query, contents));       
     }
 
     #[test]
